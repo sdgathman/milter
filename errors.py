1c92027696216ffaf08e4d0e558793c99894df3e
Stuart Gathman <stuart@gathman.org>
2012-02-25 23:17:11 +0000

    Undo % doubling

--- errors.py
+++ errors.py
@@ -6,14 +6,17 @@ import re
 import sys
 
 template_re = re.compile(r'\%([A-Za-z0-9_]*)')
+R = re.compile(r'%+')
 
 def output(DATA):
+  def getfield(name):
+    return R.sub(lambda t: '%'*((t.end()-t.start())//2),DATA.getfirst(name,''))
+    
   print "Content-type: text/html\n"
   print "<html><body>"
   filename = "/var/www/html/python/errors%s.html" % os.environ["PATH_INFO"]
   with open(filename,'r') as FILE:
-    print template_re.sub(lambda m: DATA.getfirst(m.expand(r'\1'),''),
-	FILE.read())
+    print template_re.sub(lambda m: getfield(m.expand(r'\1')), FILE.read())
   print "</body></html>"
 
 form = cgi.FieldStorage()
